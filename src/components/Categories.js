91343834c8194e1ce138bc033f05f7646d873d0a
pattWill <pattywilliams20@gmail.com>
2022-06-15 11:21:35 -0300

    agregado el botón volver a la app

---
+++ src/components/Categories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Categories(props){
+    return(
+        <React.Fragment>
+
+                    <div className="">
+                        {props.name}
+                    </div>
+
+        </React.Fragment>
+    )
+}
+export default Categories;
---
+++ src/components/UsersInDb.js
@@ -0,0 +1,67 @@
+import React,{Component} from 'react';
+
+import UserList from './User';
+
+class UsersInDb extends Component{
+
+	constructor(){
+        super()
+        this.state={
+            usersList: []
+        }   
+    }
+
+	componentDidMount()
+    {
+
+        fetch('http://localhost:3001/api/users')
+        .then(respuesta =>{
+            console.log(respuesta)
+        return respuesta.json()
+        
+        })
+        .then(users =>{
+        console.log(users)
+        this.setState({usersList: users.users})
+        })
+        .catch(error => console.log(error))
+    }
+
+
+	render(){
+
+		return(
+			<React.Fragment>
+						{/*<!-- PRODUCTS LIST -->*/}
+
+						<div className="card shadow mb-4">
+            
+							<div className="">
+								<div className="table-responsive">
+									
+									<table className="table" id="dataTable" cellSpacing="0">
+										<thead>
+											<tr>
+												<th>ID</th>
+												<th>Usuario</th>
+												<th>Email</th>
+											</tr>
+										</thead>
+										{
+                                        this.state.usersList.map((user,index)=>{
+                                            return <UserList  {...user}  key={index} />
+                                        })
+                        				}   
+										
+									</table>
+								</div>
+							</div>
+						</div>  
+						 
+			</React.Fragment>
+		)
+
+	}
+    
+}
+export default UsersInDb;
